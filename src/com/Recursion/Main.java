cbbc7aa56dddb3eeebcf581e619dd0e7da1bb059
Nitin <nsinghboss02@gmail.com>
2022-02-04 11:07:09 +0530

    Count the number of permutations in a string.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -8,6 +8,8 @@ public class Main {
         permutations("", "abc");
         ArrayList<String> list = permutationList("","abc");
         System.out.println(list);
+        int ans = permutationCount("","abcd");
+        System.out.println(ans);
 
 
     }
@@ -48,4 +50,18 @@ thats why we are taking loop till p.length()+1.
         }
         return ans;
     }
+     // returning the number of permutations of a string.
+     public static int permutationCount(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        int count=0;
+        char ch = up.charAt(0);
+        for(int i=0;i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            count = count + permutationCount(f+ch+s,up.substring(1));
+        }
+        return count;
+     }
 }
